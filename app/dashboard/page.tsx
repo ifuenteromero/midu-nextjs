3ff84019b9e9b974b97b4bdd17a5232005fdd502
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-08-14 19:03:32 +0200

    7 fetching data for revenue chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,21 @@
-const page = () => {
-	return <div>Dashboard</div>;
+import { fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { lusitana } from '../ui/fonts';
+
+const DashboardPage = async () => {
+	const revenue = await fetchRevenue();
+
+	return (
+		<main>
+			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+				Dashboard
+			</h1>
+			<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'></div>
+			<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+				<RevenueChart revenue={revenue} />
+			</div>
+		</main>
+	);
 };
 
-export default page;
+export default DashboardPage;
