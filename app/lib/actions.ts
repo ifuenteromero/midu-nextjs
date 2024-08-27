0b7cd00979736f6f2e343e420391b548b169c7f4
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-08-27 17:40:22 +0200

    12 Creating an invoice. 3. Extract the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,11 @@
 'use server';
 
-export const createInvoice = async (formData: FormData) => {};
+export const createInvoice = async (formData: FormData) => {
+	const rawFormData = {
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	};
+
+	// const rawFormData2 = Object.fromEntries(formData.entries());
+};
