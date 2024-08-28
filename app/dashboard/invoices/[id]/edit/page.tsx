e07a26f65b80ed4ce0b66b50f8096739ec7f04b6
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-08-28 16:59:11 +0200

    13 Handling 404 errors with the notFound function

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,12 +1,17 @@
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
 const Page = async ({ params: { id } }: { params: { id: string } }) => {
 	const [invoice, customers] = await Promise.all([
 		fetchInvoiceById(id),
 		fetchCustomers(),
 	]);
+
+	// not found  http://localhost:3000/dashboard/invoices/2e94d1ed-d220-449f-9f11-f0bbceed9645/edit
+
+	if (!invoice) notFound();
 
 	return (
 		<main>
